16182aee97f8b287ace54834e9d9adb5a11a98b0
dirtdk <95955656+dirtdk@users.noreply.github.com>
2022-08-12 01:33:19 +0530

    make it better

--- src/models/todo.models.js
+++ src/models/todo.models.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose')
 
 const todoSchema = new mongoose.Schema({
+    id:{ type: String, required: true },
     title: { type: String, required: true },
 })
 const Todo = mongoose.model('todo', todoSchema)
